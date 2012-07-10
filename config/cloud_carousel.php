3406e976f70ea84d92a04ab753b0e9593c8e36d6
Bill Clinton <bclinton@appunto.net>
2012-07-09 22:14:09 -0400

    first check in

---
+++ config/cloud_carousel.php
@@ -0,0 +1,9 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Default paths for cloud carousel includes.
+ *
+ */
+$config['image_path'] = '/sparks/resources/carousel-test';  // no trailing slash!!!
+$config['css_path'] = '/sparks/resources/cloud-carousel';  // no trailing slash!!!
+$config['js_path'] = '/sparks/resources/cloud-carousel';  // no trailing slash!!!
